25b16cac2efdf216574f4f5289a9d9925d194485
Aiyshwarya <aiyshwarya22@gmail.com>
2021-08-06 07:55:24 +0530

    root project created

---
+++ pollingApplication/src/test/java/com/pollingApplication/PollingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pollingApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PollingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
